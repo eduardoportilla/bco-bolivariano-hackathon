3a72c4da6342d501cfa1a2854c1b58a403a146cf
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2026-01-14 15:53:45 -0500

    feat(mobile): simplify entrypoint structure

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootNavigator } from './navigation';
 
-export function App() {
+function App() {
   return (
     <SafeAreaProvider>
       <RootNavigator />
     </SafeAreaProvider>
   );
 }
+
+export default App;
