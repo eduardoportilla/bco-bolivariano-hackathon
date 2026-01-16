4a9dda90d147c41a6dd41034196a7285ba8ce3de
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2026-01-16 12:22:03 -0500

    fix(mobile): add ESLint overrides for CommonJS config files

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      // Config files are CommonJS and don't need Babel parsing
+      files: ['*.config.js', '.eslintrc.js'],
+      parser: 'espree',
+      parserOptions: {
+        ecmaVersion: 2022,
+      },
+      env: {
+        node: true,
+      },
+    },
+  ],
 };
