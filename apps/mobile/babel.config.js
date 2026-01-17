f4943493b05a1830f15dae8201dbc609d8eb4bbd
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2026-01-16 19:28:56 -0500

    fix(mobile): add @babel/plugin-transform-export-namespace-from to resolve module exports

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -6,6 +6,8 @@ const workletsPluginOptions = {
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    // Required for "export * as namespace" syntax (eg. zod v4)
+    '@babel/plugin-transform-export-namespace-from',
     ['react-native-worklets/plugin', workletsPluginOptions],
   ],
 };
