d932b8060214a1e68df7a78846b9d28644e3abc6
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2026-01-19 18:03:56 -0500

    chore(mobile): simplify ESLint configuration by removing unnecessary overrides

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,17 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  overrides: [
-    {
-      // Config files are CommonJS and don't need Babel parsing
-      files: ['*.config.js', '.eslintrc.js'],
-      parser: 'espree',
-      parserOptions: {
-        ecmaVersion: 2022,
-      },
-      env: {
-        node: true,
-      },
+  parserOptions: {
+    babelOptions: {
+      root: __dirname,
     },
-  ],
+  }
 };
